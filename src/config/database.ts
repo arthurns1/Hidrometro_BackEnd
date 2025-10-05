50812dddc5193ef8dcebe860593dcd925adc04e7
arthurns1 <js.arthurns90@gmail.com>
2025-10-05 20:36:04 -0300

    Adicionando gitignore

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config();
 
 const sequelize = new Sequelize({
-    database: "liga_jovem_final",
+    database: process.env.DB,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
